f0aefcdd2ed0dd0d8c677108c71e6f68963425cb
Lapukhin Maksim <154962659+svcmacwork@users.noreply.github.com>
2025-12-19 11:54:49 +0300

    Delete lab8/config.php

--- lab8/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Пример конфигурации для MySQL
-define('DB_HOST', 'localhost');
-define('DB_USER', 'f1207149');
-define('DB_PASSWORD', 'Qqaazz007553?');
-define('DB_NAME', 'f1207149_login_db');
-define('DB_CHARSET', 'utf8mb4');
